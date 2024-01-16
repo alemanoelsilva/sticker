5bbfd0b809079f85d6f820dae361b3355408bafa
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2024-01-16 22:43:27 +0000

    add GetStickerById handler

--- src/routes/sticker.routes.go
+++ src/routes/sticker.routes.go
@@ -11,7 +11,7 @@ var Router = mux.NewRouter().StrictSlash(true)
 
 func RegisterStickerRoutes() {
 	Router.HandleFunc("/api/stickers", controllers.GetStickers).Methods("GET")
-	// Router.HandleFunc("/api/stickers/{id}", controllers.GetStickerById).Methods("GET")
+	Router.HandleFunc("/api/stickers/{id}", controllers.GetStickerById).Methods("GET")
 	Router.HandleFunc("/api/stickers", controllers.CreateSticker).Methods("POST")
 	// Router.HandleFunc("/api/stickers/{id}", controllers.UpdateSticker).Methods("PUT")
 	// Router.HandleFunc("/api/stickers/{id}", controllers.DeleteSticker).Methods("DELETE")
