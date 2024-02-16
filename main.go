a6d451074aaa7ed6f7a92f3b28ea18b2380adf99
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2024-02-16 02:30:44 +0000

    add health check

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"sticker/src/config"
 	"sticker/src/config/database"
 	"sticker/src/routes"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -17,10 +19,14 @@ func main() {
 	database.Connect(config.AppConfig.ConnectionString)
 	database.Migrate()
 
+	// Initialize the router
+	var Router = mux.NewRouter().StrictSlash(true)
+
 	// Register Routes
-	routes.RegisterStickerRoutes()
+	routes.RegisterStickerRoutes(Router)
+	routes.RegisterSignupRoutes(Router)
 
 	// Start the server
-	log.Println(fmt.Sprintf("Starting Server on port %s", config.AppConfig.Port))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.AppConfig.Port), routes.Router))
+	log.Println(fmt.Sprintf("Starting Server on port %d", config.AppConfig.Port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.AppConfig.Port), Router))
 }
